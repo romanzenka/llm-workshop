79c132429847a0efd1a0e0a507e33f81a09fa078
Roman Zenka <zenka.roman@mayo.edu>
2024-03-04 16:43:08 -0800

    Fixes trying to prompt llama2 better

--- tutorials/04-rag/rag.py
+++ tutorials/04-rag/rag.py
@@ -3,7 +3,7 @@ from langchain import HuggingFacePipeline
 from os import path
 import torch
 
-device = "cuda:7" if torch.cuda.is_available() else "cpu"
+device = "cuda:6" if torch.cuda.is_available() else "cpu"
 
 with open('/homes/ac.rzenka/llm/.token', 'r') as file:
     hf_token = file.read().replace('\n', '')
@@ -36,15 +36,31 @@ else:
   print("Loading existing FAISS database")
   db = FAISS.load_local("faiss_pdfs", embeddings)
 
+print("-------------------------------------")
 question = "What is RF Fold?"
+print(f"Find closest database entry to '{question}'\n")
 searchDocs = db.similarity_search(question)
 print(searchDocs[0].page_content)
+print("=====================================")
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, GenerationConfig
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer, device=device)
+MODEL_NAME = "meta-llama/Llama-2-7b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype = torch.float16)
+
+generation_config = GenerationConfig.from_pretrained(MODEL_NAME)
+generation_config.max_new_tokens = 1024
+generation_config.temperature = 0.0001
+generation_config.top_p = 0.95
+generation_config.do_sample = True
+generation_config.repetition_penalty = 1.15
+
+pipe = pipeline("text-generation", 
+                model=model, 
+                tokenizer=tokenizer, 
+                device=device, 
+                generation_config=generation_config)
 llm = HuggingFacePipeline(
    pipeline = pipe,
    model_kwargs={"temperature": 0, "max_length": 2048, "return_full_text": False},
@@ -52,10 +68,14 @@ llm = HuggingFacePipeline(
 
 from langchain.prompts import PromptTemplate
 
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible.
+template = """<s>[INST] <<SYS>>
+Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+Keep the answer as concise as possible.
 {context}
-Question: {question}
-Helpful Answer:"""
+<</SYS>>
+{question}
+[/INST]"""
 QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
 from langchain.chains import RetrievalQA
@@ -65,9 +85,17 @@ qa_chain = RetrievalQA.from_chain_type(
   retriever=db.as_retriever(),
   chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
 )
-result = qa_chain ({ "query" : "What technique proposed in 2023 can be used to predict protein folding?" })
+print("-----------------------")
+query = { "query" : "What technique proposed in 2023 can be used to predict protein folding?" };
+print(f"Question: {query['query']}")
+result = qa_chain (query)
 print(result["result"])
+print("=======================")
 
-article = qa_chain ({ "query" : "Which scientific article should I read to learn about RFdiffusion for protein folding?" })
+print("-----------------------")
+query = { "query" : "Which scientific article should I read to learn about RFdiffusion for protein folding?" }
+print(f"Question: {query['query']}")
+article = qa_chain (query)
 print(article["result"])
+print("=======================")
 
